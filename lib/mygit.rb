a7ddd3004a6017a8d1d58fdf765e19d8e1eb0ac8
Ian Vaughan <github@ianvaughan.co.uk>
2013-01-21 22:15:02 +0000

    rewrite

--- lib/mygit.rb
+++ lib/mygit.rb
@@ -1,5 +1,4 @@
 require 'httparty'
-require 'pp'
 require 'highline/import'
 
 class GitHub
@@ -15,25 +14,108 @@ class GitHub
   end
 end
 
+class FileStorage
+  CONFIG_DIR = "#{Dir.home}/.mygit"
+  CONFIG_FILE = "#{CONFIG_DIR}/config.yml"
+  REPOS_FILE = "#{CONFIG_DIR}/repos.yml"
+
+  attr_reader :repo
+
+  def initialize
+    @repo ||= YAML.load_file REPOS_FILE if File.exist?(REPOS_FILE)
+  end
+
+  class << self
+    def update filename = CONFIG_FILE
+      # Get username from prompt if file missing
+      file = File.expand_path filename
+      conf = YAML.load_file file
+      gh = GitHub.new(conf['user'], get_input("Enter password: ", '*'))
+
+      repo = gh.repos(conf['repo'])
+      keep = remove_keys repo
+      save keep
+    end
+
+    KEEP_KEYS = ['name', 'ssh_url']
+    def remove_keys array
+      keep = []
+      array.each do |a|
+        keep << a.keep_if {|k| KEEP_KEYS.include? k }
+      end
+      keep
+    end
+
+    def get_input prompt, echo = true
+      ask(prompt) {|q| q.echo = echo}
+    end
+
+    def save data
+      File.open(REPOS_FILE, "w+") {|f| f.puts(data.to_yaml) }
+      # File.open(fnm, ‘w’) { |out| YAML.dump(h, out) }
+    end
+  end
+end
+
 class Access
-  def initialize filename = '~/.mygit'
-    file = File.expand_path filename
-    conf = YAML.load_file file
-    @repo = GitHub.new(conf['user'], conf['pass']).repos(conf['repo'])
-  end
+  COL_WIDTH = 30
 
-  def list
-    @repo.each do |r|
-      spaces = ' ' * (30 - r['name'].size)
-      puts "#{r['name']} #{spaces} #{r['ssh_url']}"
+  class << self
+    def list
+      repo.each do |r|
+        spaces = ' ' * (COL_WIDTH - r['name'].size) unless r['name'].size > COL_WIDTH
+        puts "#{r['name']} #{spaces} #{r['ssh_url']}"
+      end
+    end
+
+    def find name
+      found = nil
+      repo.each do |r|
+        found = r if r['name'] == name
+      end
+      found
+    end
+
+    def repo
+      @repo ||= FileStorage.new.repo
     end
   end
+end
 
-  def find name
-    found = nil
-    @repo.each do |r|
-      found = r if r['name'] == name
+class Commands
+  def current_branch
+    `git rev-parse --abbrev-ref HEAD`
+  end
+
+  def branch_url
+    "tree/#{current_branch}"
+  end
+
+  # TODO: Use OptionsParser
+  def self.execute args
+    cmd = args.shift
+    case cmd
+    when 'update'
+      FileStorage::update
+    when 'list'       # dumps a list of all repos found
+      Access::list
+    when 'find'       # find a repo by name
+      pp Access::find args.shift
+    when 'clone'      # clone a repo by name
+      p = Access::find args.shift
+      cmd = "git clone #{p['ssh_url']}"
+      system cmd
+    when 'open'       # Opens either the current pwd, or a supplied project, GitHub project page in a browser
+      opt = args.shift
+      opt = File.basename(Dir.getwd) if opt.nil?
+      p = Access.new.find opt
+      system "open #{p['html_url']}/#{branch_url}"
+    else
+      puts 'Unknown or no command given! Options are :-'
+      File.open(__FILE__).each_line do |line|
+        puts "  " + line.sub('when','').sub('#', '->').gsub('\'','') if line.include? 'when'
+        break if line.include? 'else'
+      end
     end
-    found
   end
 end
--- mygit.gemspec
+++ mygit.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name        = "mygit"
-  s.version     = '0.0.2'
+  s.version     = '0.0.3'
   s.platform    = Gem::Platform::RUBY
   s.authors     = ["Ian Vaughan"]
   s.email       = ["mygit@ianvaughan.co.uk"]
